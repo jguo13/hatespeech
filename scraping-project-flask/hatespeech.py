0e717f62ef13e513b5f4eddbaec8692f2df82bc8
Jay <jayge2017@gmail.com>
2021-03-22 22:53:41 -0400

    second commit

--- scraping-project-flask/hatespeech.py
+++ scraping-project-flask/hatespeech.py
@@ -9,7 +9,11 @@ import csv
 
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
-
+hate_percentage = [
+    {
+        'percentage': 0
+    }
+]
 
 DEBUG = True
 
@@ -22,69 +26,72 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('pong!')
 
-@app.route('/books', methods=['GET'])
+@app.route('/get_hate', methods=['GET'])
 def all_books():
+    response_object = {'status': 'success'}
     if request.method == 'GET':
-        check_hate()
+        len_hate = check_hate()
+        response_object["percentage"] = len_hate
+        return jsonify(response_object)
 #define important variables
 
 
 def check_hate():
     print("here")
     board_name = 'pol'
-    filename = 'crawled_data.csv'
-    chinese_hate_speech_filename = "hatebase_vocab_chinese.csv"
-    asian_hate_speech_filename = "hatebase_vocab_asian.csv"
+    # filename = 'crawled_data.csv'
+    # chinese_hate_speech_filename = "hatebase_vocab_chinese.csv"
+    # asian_hate_speech_filename = "hatebase_vocab_asian.csv"
 
     ##########DONOT EDIT BELOW##################################################################
 
-    while True:
-        try:
-            print("here2")
-            df_chinese = pd.read_csv(chinese_hate_speech_filename)
-            print("here4")
-            df_asian = pd.read_csv(asian_hate_speech_filename)
-            list_id = []
-            if os.path.isfile(filename):
-                list_id = pd.read_csv(filename)["post_id"].to_list()
-            
-            df_total = pd.concat([df_chinese, df_asian])
-            list_hate = df_total["term"]
+    # while True:
+    try:
+        # df_chinese = pd.read_csv(chinese_hate_speech_filename)
+        # df_asian = pd.read_csv(asian_hate_speech_filename)
+        # list_id = []
+        # if os.path.isfile(filename):
+        #     list_id = pd.read_csv(filename)["post_id"].to_list()
+        
+        # df_total = pd.concat([df_chinese, df_asian])
+        # list_hate = df_total["term"]
+        list_hate = ['ABCs', 'ABC', 'spinks', 'spink', 'slopey', 'winks', 'slopes', 'slopies', 'slants', 'slopeheads', 'slant eyes', 'sideways vaginas', 'sideways cooters', 'sideways pussies', 'coolies', 'chonkies', 'chunkies', 'Chinese wetbacks', 'ching chongs', 'chinigs', 'chink a billies', 'chiggers', 'celestials', 'bamboo coons', 'chinig', 'sideways cooter', 'slopehead', 'chink a billy', 'Chinese wetback', 'bamboo coon', 'ching chong', 'coolie', 'slopy', 'chonky', 'chunky', 'sideways pussy', 'sideways vagina', 'chigger', 'slope', 'slant', 'slant eye', 'wink', 'celestial', 'whoriental', 'whorientals', 'gooky eyes', 'gooklets', 'gooklet', 'gookettes', 'gookette', 'gook eyed', 'gookies', 'gookie', 'goloids', 'goloid', 'ginks', 'gink', 'dog eaters', 'dog eater', 'yellow invaders', 'rice niggers', 'yellow invader', 'rice nigger']
+        # col_names = ["text_comment", "datetime", "post_id"]
+        # df_asian_hate = pd.DataFrame(columns = col_names)
+        # get the board we want
+        board = basc_py4chan.Board(board_name)
+        # select the first thread on the board
+        all_thread_ids = board.get_all_thread_ids()
+        # thread_list = []
+        thread_count = 0
+        hate_count = 0
+        for id in all_thread_ids:
 
-            col_names = ["text_comment", "datetime", "post_id"]
-            df_asian_hate = pd.DataFrame(columns = col_names)
-            print("here3")
-            # get the board we want
-            board = basc_py4chan.Board(board_name)
-            print("this is board", board)
-            # select the first thread on the board
-            all_thread_ids = board.get_all_thread_ids()
-            thread_list = []
+            first_thread_id = id
+            thread = board.get_thread(first_thread_id)
+            if thread == None:
+                continue
+            topic = thread.topic
+            for thread in thread.all_posts:
+                thread_count += 1
+                if any(s.lower() in thread.comment.lower() for s in list_hate):
+                    hate_count += 1
+                    # temp_dict = {}
+                    # temp_dict["text_comment"] = thread.text_comment
+                    # temp_dict["datetime"] = thread.datetime
+                    # temp_dict["post_id"] = thread.post_id
+                    # df_asian_hate = df_asian_hate.append(temp_dict, ignore_index = True)
 
-            for id in all_thread_ids:
+        percentage = "{:.2%}".format(hate_count / thread_count)
+        return percentage
+        # if not os.path.isfile(filename):
+        #     df_asian_hate.to_csv(filename, header='column_names')
 
-                first_thread_id = id
-                thread = board.get_thread(first_thread_id)
-                print("this is thread: ", thread)
-                if thread == None:
-                    continue
-                topic = thread.topic
-                for thread in thread.all_posts:
-                    if any(s.lower() in thread.comment.lower() for s in list_hate) and thread.post_id not in list_id:
-                        temp_dict = {}
-                        temp_dict["text_comment"] = thread.text_comment
-                        temp_dict["datetime"] = thread.datetime
-                        temp_dict["post_id"] = thread.post_id
-                        df_asian_hate = df_asian_hate.append(temp_dict, ignore_index = True)
-
-            if not os.path.isfile(filename):
-                df_asian_hate.to_csv(filename, header='column_names')
-
-            else: # else it exists so append without writing the header
-                df_asian_hate.to_csv(filename, mode='a', header=False)
-        except:
-            time.sleep(2)
-            continue
+        # else: # else it exists so append without writing the header
+        #     df_asian_hate.to_csv(filename, mode='a', header=False)
+    except :
+        return "error with the server, please refresh"
+            # continue
 
 
 if __name__ == '__main__':
